cb59e38f90889a57fb29d802f2e3e682bb79487e
Gotfrid <gotfridwallenstein@gmail.com>
2024-01-13 02:07:31 +0300

    1. Synchronizing access to shared resources [#1096]

---
+++ src/main/java/ru/job4j/Cache.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public final class Cache {
+    private static Cache cache;
+
+    private Cache() {
+    }
+
+    public synchronized static Cache getInstance() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
