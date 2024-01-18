4b5b4072e0107519fa2607eea9d60cf56e7e5bf3
Gotfrid <gotfridwallenstein@gmail.com>
2024-01-18 22:53:04 +0300

    3. Money transfers with AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,44 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public synchronized void delete(int id) {
+        if (getById(id).isPresent()) {
+            accounts.remove(id);
+        }
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> source = getById(fromId);
+        Optional<Account> target = getById(toId);
+        if (source.isPresent() && target.isPresent()) {
+            if (source.get().amount() >= amount) {
+                update(new Account(fromId, source.get().amount() - amount));
+                update(new Account(toId, target.get().amount() + amount));
+                return true;
+            }
+        }
+        return false;
+    }
+}
