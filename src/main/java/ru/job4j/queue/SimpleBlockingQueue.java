80913610cf0d8199503bd4af1ba055d2bd4cdde8
Gotfrid <gotfridwallenstein@gmail.com>
2024-01-26 22:48:11 +0300

    1. Implement Producer Consumer pattern. [#1098]

---
+++ src/main/java/ru/job4j/queue/SimpleBlockingQueue.java
@@ -0,0 +1,46 @@
+package ru.job4j.queue;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    private final int capacity;
+
+    public SimpleBlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public synchronized void offer(T value) {
+        while (queue.size() >= capacity) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        queue.add(value);
+        System.out.println("Value added to the queue: " + value);
+        notify();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (isEmpty()) {
+            wait();
+        }
+        T value = queue.poll();
+        notify();
+        return value;
+    }
+}
---
+++ src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.queue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleBlockingQueueTest {
+    @Test
+    void whenPollAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        queue::offer
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isNotNull()
+                .containsExactly(0, 1, 2, 3, 4);
+    }
+}
