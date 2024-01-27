185ce261c71d7662c5f5b07ae1d2b35bf35f83b0
Gotfrid <gotfridwallenstein@gmail.com>
2024-01-27 21:20:07 +0300

    2. Stop the consumer [#66825] fix

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -4,7 +4,7 @@ import ru.job4j.queue.SimpleBlockingQueue;
 
 public class ParallelSearch {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
         final Thread consumer = new Thread(
                 () -> {
@@ -12,26 +12,29 @@ public class ParallelSearch {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
                 }
         );
         consumer.start();
-        new Thread(
+
+        final Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
-                        queue.offer(index);
                         try {
+                            queue.offer(index);
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
                 }
-        ).start();
+        );
 
+        producer.start();
+        producer.join();
         consumer.interrupt();
     }
 }
