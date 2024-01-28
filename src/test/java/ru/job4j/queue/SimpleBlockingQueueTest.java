368707dd49fd48a1396a6795937c6018a5f6eb16
Gotfrid <gotfridwallenstein@gmail.com>
2024-01-29 01:51:25 +0300

    Junit тест for blocking queue. [#68589]

--- src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/queue/SimpleBlockingQueueTest.java
@@ -43,4 +43,40 @@ class SimpleBlockingQueueTest {
         assertThat(buffer).isNotNull()
                 .containsExactly(0, 1, 2, 3, 4);
     }
+
+    @Test
+    void whenOfferAndPollFromOneSlotQueue() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 10).forEach(
+                        i -> {
+                            try {
+                                queue.offer(i);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isNotNull()
+                .containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
 }
